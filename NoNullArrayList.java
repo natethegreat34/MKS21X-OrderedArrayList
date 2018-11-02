105b787436d90750a1e1797c01107d5928b36e48
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-11-02 11:38:43 -0400

    Finished Null and started ordered

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -9,33 +9,25 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     super (startingCapacity);
   }
 
-public set (int index, T element){
+public T set (int index, T element){
   if (element == null){
     throw new IllegalArgumentException ();
   }
-  super.set(index, element)
+else{ return super.set(index, element);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+public boolean add (T element){
+  if (element == null){
+    throw new IllegalArgumentException ();
+  }
+else{ return super.add(element);
+}
+}
+public void add (int index, T element){
+  if (element == null){
+    throw new IllegalArgumentException ();
+  }
+else{ super.add(index, element);
+}
+}
 }
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,5 +1,25 @@
 
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public OrderedArrayList (){
+    super ();
+  }
+  public OrderedArrayList (int startingCapacity){
+    super (startingCapacity);
+  }
+  public void sort (){
+    for (int i = 0; i + 1< this.size(); i ++){
+      int val = this.get(i).compareTo(this.get(i +1));
+      if (val > 0){
+      T first = this.get(i);
+      T second = this.get(i + 1);
+      this.set (i,second);
+      this.set(i+1, first);
+      }
+    }
+  }
+  //public add (T element){
+    //for (int i = 0; i < )
+  //}
 
 
 
