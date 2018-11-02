d33fab07eee4331595ceb045625a415bb4e3d33a
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-11-02 11:47:17 -0400

    Think i got progress

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,20 +6,20 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList (int startingCapacity){
     super (startingCapacity);
   }
-  public void sort (){
-    for (int i = 0; i + 1< this.size(); i ++){
-      int val = this.get(i).compareTo(this.get(i +1));
-      if (val > 0){
-      T first = this.get(i);
-      T second = this.get(i + 1);
-      this.set (i,second);
-      this.set(i+1, first);
+
+
+  public void add (T element){
+      for (int i = 0; i + 1< this.size(); i ++){
+        int val = element.compareTo(this.get(i +1));
+        if (val > 0){
+        super.add(i, element);
       }
-    }
-  }
-  //public add (T element){
-    //for (int i = 0; i < )
-  //}
+        if (val <= 0)
+        {super.add(i + 1, element);;}
+
+      }}
+
+
 
 
 
